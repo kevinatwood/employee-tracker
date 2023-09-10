ae49f1fd7d33c379dc15364b2c5773c455ada064
kevinatwood <atwoodkevin22@gmail.com>
2023-09-10 14:40:06 -0500

    added database seeds, seeded db, added inquirer prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,22 +4,22 @@ VALUES  ('Sales'),
         ('Finance'),
         ('Legal');
 
-INSERT INTO role (title, salary)
-VALUES  ('Sales Lead','100000'),
-        ('Salesperson','80000'),
-        ('Lead Engineer','150000'),
-        ('Software Engineer','120000'),
-        ('Accout Manager','160000'),
-        ('Accountant','125000'),
-        ('Legal Team Lead','250000'),
-        ('Lawyer','190000');
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Sales Lead','100000', 1),
+        ('Salesperson','80000', 1),
+        ('Lead Engineer','150000', 2),
+        ('Software Engineer','120000', 2),
+        ('Accout Manager','160000', 3),
+        ('Accountant','125000', 3),
+        ('Legal Team Lead','250000', 4),
+        ('Lawyer','190000', 4);
 
-INSERT INTO employee (first_name, last_name)
-VALUES  ('John', 'Doe'),
-        ('Mike', 'Chan'),
-        ('Ashley','Rodriguez'),
-        ('Kevin','Tupik'),
-        ('Kunal', 'Singh'),
-        ('Malia', 'Brown'),
-        ('Sarah', 'Lord'),
-        ('Tom', 'Allen');
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('John', 'Doe', 1 , NULL),
+        ('Mike', 'Chan', 2, 1),
+        ('Ashley','Rodriguez', 3, NULL),
+        ('Kevin','Tupik', 4, 3),
+        ('Kunal', 'Singh', 5, NULL),
+        ('Malia', 'Brown', 6, 5),
+        ('Sarah', 'Lord', 7, NULL),
+        ('Tom', 'Allen', 8, 7);
--- node/server.js
+++ node/server.js
@@ -3,20 +3,53 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: process.env.DB_USER,
     password:   process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 }, 
 console.log('Conected to database!'))
 
-// const startMenu = () =>{
-// inquirer .prompt([
-//     {
-//         type: "list",
-//         name: "menu",
-//         message: "What would you like to do?",
-//         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
-//     }
-// ]).then (response){
-//     switch
-// }}
+
+db.connect(function (err) {
+    if (err) throw err;
+    startMenu();
+});
+
+const startMenu = () => {
+inquirer .prompt([
+    {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+    }
+]).then ((res) => {
+    switch(res){
+        case 'View All Employees':
+        viewEmployees()
+        break;
+        case 'Add Employee':
+
+        break;
+        case 'Update Employee Role':
+
+        break;
+        case 'View All Roles':
+
+        break;
+        case 'Add Role':
+
+        break;
+        case 'View All Departments':
+
+        break;
+        case 'Add Department':
+
+        break;
+        case 'Quit':
+
+        break;
+    }
+})}
+
+const viewEmployees = () => console.log("hello");
