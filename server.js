bddbed747a32c4e004170a46f258456bfe1f3e2a
kevinatwood <atwoodkevin22@gmail.com>
2023-09-12 20:48:27 -0500

    changed credentials

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Fuckface69',
+    password: '',
     database: 'employees_db',
 },
     console.log('Conected to database!'));
